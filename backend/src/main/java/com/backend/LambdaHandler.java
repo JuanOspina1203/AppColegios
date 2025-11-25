c3b545a54b9c3e0ae43b5f4aff612a08f0897170
Juan-Ospina-Delgadillo <juan.ospina@srfconsultores.com>
2025-11-25 14:26:49 -0500

    3

---
+++ backend/src/main/java/com/backend/LambdaHandler.java
@@ -0,0 +1,14 @@
+package com.backend;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+public class LambdaHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent event, Context context) {
+        return bookController.handleRequest(event, context); juanospina1107
+    }
+}
